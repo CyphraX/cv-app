303bf38c0d5fa9d5e73ae80a0647ea832d860fe1
Philipp Saal <saalphilipp96@gmail.com>
2025-11-28 09:03:20 +0100

    Upgrade to stable versions: Spring Boot 3.5.8, Kotlin 2.0.21, Gradle 8.14, Jib 3.5.1

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    id("org.springframework.boot") version "3.2.5"
-    id("io.spring.dependency-management") version "1.1.4"
-    kotlin("jvm") version "1.9.23"
-    kotlin("plugin.spring") version "1.9.23"
-    id("com.google.cloud.tools.jib") version "3.4.1"
+    id("org.springframework.boot") version "3.5.8"
+    id("io.spring.dependency-management") version "1.1.7"
+    kotlin("jvm") version "2.0.21"
+    kotlin("plugin.spring") version "2.0.21"
+    id("com.google.cloud.tools.jib") version "3.5.1"
 }
 
 group = "com.philippsaal"
